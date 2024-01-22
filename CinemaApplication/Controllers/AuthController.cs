7f1bcefbe25b09ce144c881487b0d80ef6c4466d
jimdimas <jimdimas6@gmail.com>
2024-01-23 01:10:50 +0200

    created sign-up/sign-in forms

---
+++ CinemaApplication/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaApplication.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
