6b649b254f29815dce6b03fa47ae2630611fb22b
jimdimas <jimdimas6@gmail.com>
2024-01-24 18:41:18 +0200

    added screening controller and view with get/post per movie

---
+++ CinemaApplication/Controllers/ScreeningController.cs
@@ -0,0 +1,43 @@
+﻿using CinemaApplication.Data;
+using CinemaApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinemaApplication.Controllers
+{
+    public class ScreeningController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public ScreeningController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Index(String MovieName)
+        {
+            TempData["MovieName"] = MovieName;
+            IEnumerable<Screening> movieScreenings = _db.Screenings.Where(s => s.Movie.MovieName == MovieName && s.Time>DateTime.Now).Include(s=> s.Cinema);
+            return View(movieScreenings);
+        }
+        [HttpPost]
+        public IActionResult Create(String MovieName,String CinemaName,DateTime Time)
+        {
+            Cinema cinema = _db.Cinemas.FirstOrDefault(c=>c.Name == CinemaName);
+            Movie movie = _db.Movies.FirstOrDefault(m=>m.MovieName==MovieName);
+            ContentAdmin contentAdmin = _db.Set<ContentAdmin>().FirstOrDefault(u => u.Username.Equals("cadmin"));
+            Screening screening = new Screening();
+            screening.Movie = movie;
+            screening.ContentAdmin = contentAdmin;
+            screening.Cinema = cinema;
+            screening.Time = Time;
+            _db.Screenings.Add(screening);
+            _db.SaveChanges();
+            return RedirectToAction("Index", new {MovieName=MovieName});
+        }
+    }
+}
