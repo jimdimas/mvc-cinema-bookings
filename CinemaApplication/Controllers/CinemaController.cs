ac82b180998f8fd27e4ffd25c5bc1b63b42dee58
jimdimas <jimdimas6@gmail.com>
2024-02-08 20:00:58 +0200

    create cinema through index view

--- CinemaApplication/Controllers/CinemaController.cs
+++ CinemaApplication/Controllers/CinemaController.cs
@@ -15,5 +15,29 @@ namespace CinemaApplication.Controllers
             IEnumerable<Cinema> cinemas = _db.Cinemas;
             return View(cinemas);
         }
+
+        [HttpPost]
+        public IActionResult Create(String Name,int Seats,Boolean ThreeDimensions)
+        {
+            Cinema cinema = new Cinema();
+            cinema.Name = Name;
+            cinema.Seats = Seats;
+            cinema.ThreeDim = (ThreeDimensions) ? "Yes" : "No";
+            _db.Cinemas.Add(cinema);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(String name)
+        {
+            Cinema cinema = _db.Cinemas.Find(name);
+            if (cinema != null)
+            {
+                _db.Remove(cinema);
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
     }
 }
