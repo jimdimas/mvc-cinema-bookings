4b209d283642663a78109de3ab07f157cfafa05d
jimdimas <jimdimas6@gmail.com>
2024-02-08 19:29:39 +0200

    cinema controller and index list view

---
+++ CinemaApplication/Controllers/CinemaController.cs
@@ -0,0 +1,19 @@
+﻿using CinemaApplication.Data;
+using CinemaApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaApplication.Controllers
+{
+    public class CinemaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CinemaController(ApplicationDbContext db) {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Cinema> cinemas = _db.Cinemas;
+            return View(cinemas);
+        }
+    }
+}
---
+++ CinemaApplication/Migrations/20240131205409_AddAllUpdatesToDb.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CinemaApplication.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAllUpdatesToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
